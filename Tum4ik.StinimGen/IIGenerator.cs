90cc23c1fa6594e41cb4fe23ad93298e4321b21d
Yevheniy Tymchishin <timchishinevgeniy@gmail.com>
2024-01-24 20:40:15 +0200

    Added System.Composition.Hosting (#23)

--- Tum4ik.StinimGen/IIGenerator.cs
+++ Tum4ik.StinimGen/IIGenerator.cs
@@ -21,6 +21,7 @@ internal sealed partial class IIGenerator : IIncrementalGenerator
   public void Initialize(IncrementalGeneratorInitializationContext context)
   {
     //System.Diagnostics.Debugger.Launch();
+
     var interfacesProvider = context.SyntaxProvider.ForAttributeWithMetadataName(
       s_iiForAttributeFullName,
       static (node, _) => node is InterfaceDeclarationSyntax interfaceDeclaration
